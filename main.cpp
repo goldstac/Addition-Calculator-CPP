3e44ff353dc1e8f9470ae855d36ca9b43e644472
Li Productions <172457372+goldstac@users.noreply.github.com>
2025-11-04 10:31:04 +0400

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int first_number;
+    int second_number;
+    cout << "Enter First Number\n";
+    cin >> first_number;
+    cout << "Enter Second Number \n";
+    cin >> second_number;
+    int calculate;
+    calculate = first_number + second_number;
+    cout << "Total Is" << " " << calculate;
+    return 0;
+}
